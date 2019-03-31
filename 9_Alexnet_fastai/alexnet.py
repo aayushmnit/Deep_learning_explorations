baddcd55fe183c787a35274f4ead178d9ae29ee4
aayushmnit <aayushmnit@gmail.com>
2019-03-31 18:40:22 -0500

    Adding Alexnet on cats and dog

---
+++ 9_Alexnet_fastai/alexnet.py
@@ -0,0 +1,65 @@
+# Import necessary packages
+import torch.nn as nn
+import numpy as np
+
+
+class AlexNet(nn.Module):
+    def __init__(self, features, n_class=1000):
+        super(AlexNet, self).__init__()
+        self.features = features
+        # FC=>ACT=>DO=>FC=>SOFTMAX
+        self.classifier = nn.Sequential(
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(),
+            nn.BatchNorm2d(4096),
+            nn.Dropout(p = 0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU(),
+            nn.BatchNorm2d(4096),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096, n_class)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+
+def make_layers():
+    layers = []
+    in_channels = 3
+    
+    ## CONV=>ACT=>BN=>POOL=>DO
+    layers += [nn.Conv2d(in_channels, out_channels = 96, kernel_size = 11, stride = 4, padding = 0)]
+    layers += [nn.ReLU()]
+    layers += [nn.BatchNorm2d(96)]
+    layers += [nn.MaxPool2d(kernel_size = 3, stride = 2)]
+    layers += [nn.Dropout(p = 0.25)]
+
+    ## CONV=>ACT=>BN=>POOL=>DO
+    layers += [nn.Conv2d(96, out_channels = 256, kernel_size = 5, stride = 1, padding = 2)]
+    layers += [nn.ReLU()]
+    layers += [nn.BatchNorm2d(256)]
+    layers += [nn.MaxPool2d(kernel_size = 3, stride = 2)]
+    layers += [nn.Dropout(p = 0.25)]
+        
+    ## ((CONV=>ACT=>BN)x3
+    in_channels = 256
+    for op in [384, 384, 256]:
+        layers += [nn.Conv2d(in_channels, op, kernel_size = 3, padding = 1)]
+        layers += [nn.ReLU()]
+        layers += [nn.BatchNorm2d(op)]
+        in_channels = op
+    
+    ## POOL=>DO
+    layers += [nn.MaxPool2d(kernel_size = 3, stride = 2)]
+    layers += [nn.Dropout(p=0.25)]
+    return nn.Sequential(*layers)
+
+
+def MiniVGGNet(batch_norm=True, **kwargs):
+    model = AlexNet(make_layers(), **kwargs)
+    return model
+
---
+++ 9_Alexnet_fastai/alexnet_catvsdog.py
@@ -0,0 +1,89 @@
+# USAGE
+"""
+python minivggnet_cifar.py \
+--input_path "../data/cifar10" \
+--output_path "./" \
+--epochs 10 \
+--learning_rate 1e-3\
+--batch_size 64 \
+--batch_norm True
+"""
+
+# Import the necessary packages
+import click
+import numpy as np
+import matplotlib.pyplot as plt
+from minivggnet import *
+from fastai.vision import ClassificationInterpretation
+from fastai.vision.data import ImageItemList
+from fastai.basic_train import Learner
+from fastai.metrics import accuracy
+from pathlib import Path
+from sklearn.metrics import classification_report
+
+
+@click.command()
+@click.option(
+    "--input_path", "-ip", default="./", required=True, help="Path to CIFAR10 data"
+)
+@click.option(
+    "--output_path",
+    "-op",
+    default="./",
+    required=True,
+    help="path to the output loss/accuracy plot",
+)
+@click.option("--batch_size", "-bs", default=64, help="Batch size")
+@click.option("--epochs", "-e", default=10, help="Number of epochs")
+@click.option("--learning_rate", "-lr", default=1e-2, help="Learning Rate")
+@click.option(
+    "--batch_norm", "-bn", default=True, type=bool, help="Batch normalization"
+)
+def run_minivggnet(
+    input_path, output_path, batch_size, epochs, learning_rate, batch_norm
+):
+
+    path = Path(input_path)
+
+    # Creating Databunch
+    data = (
+        ImageItemList.from_folder(path)
+        .split_by_folder(train="train", valid="test")
+        .label_from_folder()
+        .transform(tfms=None, size=32)
+        .databunch(bs=batch_size)
+    )
+
+    # Defining the learner
+    minivgg_learner = Learner(
+        data=data,
+        model=MiniVGGNet(batch_norm=batch_norm, size=32, n_class=data.c),
+        loss_func=nn.CrossEntropyLoss(),
+        metrics=accuracy,
+    )
+
+    # Training the model
+    minivgg_learner.fit_one_cycle(epochs, learning_rate)
+
+    val_acc = int(
+        np.round(minivgg_learner.recorder.metrics[-1][0].numpy().tolist(), 3) * 1000
+    )
+
+    # Saving the model
+    minivgg_learner.save("minivgg_cifar10_stg_1_" + str(val_acc))
+
+    # Evaluation
+    print("Evaluating Network..")
+    interp = ClassificationInterpretation.from_learner(minivgg_learner)
+    print(classification_report(interp.y_true, interp.pred_class))
+
+    # Plotting train and validation loss
+    minivgg_learner.recorder.plot_losses()
+    plt.savefig(output_path + "/loss.png")
+
+    minivgg_learner.recorder.plot_metrics()
+    plt.savefig(output_path + "/metric.png")
+
+
+if __name__ == "__main__":
+    run_minivggnet()

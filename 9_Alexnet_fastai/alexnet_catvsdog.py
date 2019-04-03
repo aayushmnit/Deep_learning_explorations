1efff14e73a63e1c0179250674e4dacf5d88d8fb
aayushmnit <aayushmnit@gmail.com>
2019-04-03 10:46:25 -0500

    Adding get_transform in import for execution script

--- 9_Alexnet_fastai/alexnet_catvsdog.py
+++ 9_Alexnet_fastai/alexnet_catvsdog.py
@@ -14,6 +14,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from alexnet import *
 from fastai.vision import ClassificationInterpretation
+from fastai.vision.transform import get_transforms
 from fastai.vision.data import ImageDataBunch
 from fastai.vision.data import get_image_files
 from fastai.basic_train import Learner
